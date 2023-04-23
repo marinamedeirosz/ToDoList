d06f0702a8285cf8a11c35fe069bb4a009859f2d
marinamedeirosz <marinapaffer@gmail.com>
2023-04-23 13:30:00 -0300

    fonts

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import Tela1 from './src/telas/Tela1';
 import Tela2 from './src/telas/Tela2';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'SigmarOne': require('./assets/fonts/SigmarOne.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <Tela1/>
+    <Tela2/>
   );
 }
 
