f853cf1d1775024724be7aaeb89bbd8d1356d061
marinamedeirosz <marinapaffer@gmail.com>
2023-04-23 19:16:32 -0300

    loading screen

--- src/telas/TelaCarregamento.js
+++ src/telas/TelaCarregamento.js
@@ -1,7 +1,23 @@
 import { View, StyleSheet, Image, Text, TouchableOpacity, Touchable} from "react-native";
+import loading from '../../assets/loading.gif'
 
 export default function TelaCarregamento() {
   return (
-    <View></View>
+    <View style={styles.container}>
+      <Image style={styles.imagem} source={loading}></Image>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, 
+  imagem: {
+    width: '70%',
+    height: '30%'
+  }
+})
