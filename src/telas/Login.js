011bd21ac7c6642cc0a6659b3d7d14e68a6603bb
marinamedeirosz <marinapaffer@gmail.com>
2023-05-21 18:11:03 -0300

    ..

--- src/telas/Login.js
+++ src/telas/Login.js
@@ -1,29 +1,55 @@
-import { View, StyleSheet, SafeAreaView } from "react-native";
-import CaixaTexto from '../componentes/MyTextInput'
-
+import { View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+//import CaixaTexto from '../componentes/MyTextInput'
+import { TextInput } from "react-native";
 
 export default function Login({ navigation }) {
-  return (
-    <SafeAreaView>
-        <View style={styles.container}>
-            <CaixaTexto 
-            placeholder='email' 
-            value={email} 
-            onChangeText={setEmail} />
-            <CaixaTexto 
-            placeholder='senha'
-            secureTextEntry 
-            value={password} 
-            onChangeText={setPassword} />
-        </View>
-    </SafeAreaView>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    autoCapitalize="none"
+                    placeholderTextColor="#727272"
+                    placeholder='email'
+                    value={email}
+                    onChangeText={setEmail} />
+                <TextInput
+                    autoCapitalize="none"
+                    placeholderTextColor="#727272"
+                    style={styles.input}
+                    placeholder='senha'
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword} />
+                <TouchableOpacity style={styles.botao} onPress={() =>
+                    navigation.navigate('Home')}>Entrar no App</TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    botao: {
+        width: '33%',
+        height: '10%',
+        backgroundColor: 'lightblue'
+    },
+    input: {
+        borderRadius: 8,
+        borderColor: '#1D013F',
+        paddingHorizontal: 8,
+        color: '#000',
+        borderWidth: 1,
+        width: '100%',
+        height: 50,
+        marginBottom: 16,
+    }
 })
