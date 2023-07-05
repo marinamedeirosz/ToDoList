f5ef5667b66845a7516286d4ce8355636b516300
Marina Medeiros <79525275+marinamedeirosz@users.noreply.github.com>
2023-07-05 14:11:09 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHcxdMSW6oDfa10zt3HdcWwTzrPsiP-mo",
-  authDomain: "jogodamemoriatepi.firebaseapp.com",
-  projectId: "jogodamemoriatepi",
-  storageBucket: "jogodamemoriatepi.appspot.com",
-  messagingSenderId: "153720798597",
-  appId: "1:153720798597:web:335295c2b29415195a98ec",
-  measurementId: "G-YYW17EXXBR"
+  
 };
 
 
